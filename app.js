3e60799509509cd777b8a3bbab17c618fb0e58ad
BoDee Angus <nyx0hemir@gmail.com>
2021-06-24 14:28:10 -0600

    added inquirer

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+const inquirer = require("inquirer");
 const fs = require("fs");
 const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
 
-fs.writeFile("index.html", generatePage(name, github), err => {
-    if (err) throw err;
+//const pageHTML = generatePage(name, github);
 
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
+//fs.writeFile("index.html", generatePage(name, github), err => {
+//    if (err) throw err;
+
+//    console.log("Portfolio complete! Check out index.html to see the output!");
+//});
